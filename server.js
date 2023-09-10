00c9a091aadbad1b7128b846f192510460d584a1
Matin <mnasirii829@gmail.com>
2023-09-10 18:30:58 +0330

    Fix An Problem!

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const app = express()
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
 
+// Download Resume
 app.get('/downloadResume', (req, res) => res.download('public/pdf/MatinNasiriAndroid.pdf'))
-app.get('/', (req, res) => res.render('index', JSON.parse(process.env.CONFIG) ))
 
+// Main
+app.get('/', (req, res) => res.render('index', JSON.parse(process.env.CONFIG)))
+
+// 404
 app.use((req, res, next) => res.redirect('/'))
-
 
 
 app.listen(3003,()=> console.log("Server On 3003 Is Running..."))
