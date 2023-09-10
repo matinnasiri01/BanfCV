d0c2fd4174a71823437c5648523ec0a195118225
Matin <mnasirii829@gmail.com>
2023-09-10 12:23:47 +0330

    lunch!

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.use('/', require('./routes/routes'))
 
-app.get("/", (req, res) => res.send(Date()))
 
-// Start Listening!
 app.listen(3000,()=> console.log("Server On 3000 Is Running..."))
